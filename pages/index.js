dd21920bf3b9f73ed871741163f16fce5e835600
Tim Paisley <tim.paisley@wcc.govt.nz>
2021-07-31 17:49:55 +1200

    Update Head with icon links and manifest

--- pages/index.js
+++ pages/index.js
@@ -62,8 +62,20 @@ export default function Home() {
   return (
     <Layout>
       <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Cards" />
+        <meta name="keywords" content="cards" />
         <title>Cards</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link href="/favicon.ico" rel="icon" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/152.png"></link>
+        <meta name="theme-color" content="#fff" />
       </Head>
 
       <Header />
