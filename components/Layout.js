ba2d9cb5e7fcb2170563ef77dcccb282bcd4070f
Tim Paisley <tim.paisley@wcc.govt.nz>
2021-09-04 15:06:29 +1200

    Move Head from _app to Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,26 @@
+import Head from 'next/head'
+
 export default function Layout({ children }) {
-  return <div className="flex h-screen flex-col max-w-sm m-auto">{children}</div>
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Cards" />
+        <meta name="keywords" content="cards" />
+        <title>Cards</title>
+        <link href="/favicon.ico" rel="icon" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/152.png"></link>
+        <meta name="theme-color" content="#fff" />
+      </Head>
+
+      <div className="flex h-screen flex-col max-w-sm m-auto">{children}</div>
+    </>
+  )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,7 @@
 import 'tailwindcss/tailwind.css'
 
-import Head from 'next/head'
-
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
-        <meta name="description" content="Cards" />
-        <meta name="keywords" content="cards" />
-        <title>Cards</title>
-        <link href="/favicon.ico" rel="icon" />
-
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/152.png"></link>
-        <meta name="theme-color" content="#fff" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
