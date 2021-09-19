401e558e429af16e2e6b2f714db68d06bc421012
Tim Paisley <tim.paisley@wcc.govt.nz>
2021-09-19 12:01:14 +1200

    Explicitly import strategy to enable custom strategies in the future

--- components/DragDrop/Container.js
+++ components/DragDrop/Container.js
@@ -1,14 +1,15 @@
 import { useDroppable } from '@dnd-kit/core'
-import { horizontalListSortingStrategy, SortableContext } from '@dnd-kit/sortable'
+import { SortableContext } from '@dnd-kit/sortable'
 
 import Row from '../Row'
 import Item from './Item'
+import { strategy } from './Strategy'
 
 export default function Container({ id, highlight = false, items, itemMap, activeId }) {
   const { setNodeRef } = useDroppable({ id })
 
   return (
-    <SortableContext id={id} items={items} strategy={horizontalListSortingStrategy}>
+    <SortableContext id={id} items={items} strategy={strategy}>
       <div ref={setNodeRef}>
         <Row
           highlight={highlight}
