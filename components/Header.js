5a107ada85ff8d5fdfebff6871b87681235cab3a
Tim Paisley <tepaisley@gmail.com>
2023-08-19 13:04:27 +1200

    Restyle header

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,25 @@
-import { faPaw } from '@fortawesome/free-solid-svg-icons'
+import { faPaw, faUndo } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
 
 export default function Header({ reset }) {
   return (
-    <div className="relative">
+    <div className="relative mb-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center py-5 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <Link href="/">
-              <a>
-                <span className="sr-only">Cards</span>
-                <FontAwesomeIcon icon={faPaw} className="h-8 w-auto" />
+              <a className="flex items-center font-black uppercase">
+                <FontAwesomeIcon icon={faPaw} className="h-4 w-auto mr-2" />
+                Cards
               </a>
             </Link>
           </div>
 
           <div className="flex items-center justify-end md:flex-1 lg:w-0">
-            <button onClick={reset}>Reset</button>
+            <button onClick={reset}>
+              <FontAwesomeIcon icon={faUndo} className="h-4 w-auto" />
+            </button>
           </div>
         </div>
       </div>
